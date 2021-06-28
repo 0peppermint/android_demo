c2221f804319c0e155a7f0ff58ed9494ab31f218
attachBaseContext <745462171@qq.com>
2021-06-28 16:31:18 +0800

    opt:更新lifecycle配置

--- componentMVP/src/main/java/com/demo/componentmvp/component/BaseComponentExt.kt
+++ componentMVP/src/main/java/com/demo/componentmvp/component/BaseComponentExt.kt
@@ -1,9 +1,13 @@
 package com.demo.componentmvp.component
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.LifecycleOwner
 import com.demo.componentmvp.lifecycle.IComponentLifeCycleListener
 
 
-fun AppCompatActivity.bindLifeCycle(componentLifeCycleListener: IComponentLifeCycleListener) {
-    this.lifecycle.addObserver(componentLifeCycleListener)
+fun Activity.bindLifeCycle(componentLifeCycleListener: IComponentLifeCycleListener) {
+    val lifecycle = (this as? LifecycleOwner)?.lifecycle
+    lifecycle?:throw IllegalArgumentException("activity must be LifecycleOwner.")
+    lifecycle.addObserver(componentLifeCycleListener)
 }
