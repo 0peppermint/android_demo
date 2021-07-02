c5b0ac51baeefbb7794ef92fdf35dbe8ee5a14a6
attachBaseContext <745462171@qq.com>
2021-07-02 15:32:22 +0800

    bugfix:修一波闪屏的bug

--- app/src/main/java/com/demo/mashiro/MainActivity.kt
+++ app/src/main/java/com/demo/mashiro/MainActivity.kt
@@ -2,10 +2,15 @@ package com.demo.mashiro
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.demo.transition.BaseTransitionActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        btn_transition_experiment.setOnClickListener {
+            BaseTransitionActivity.startUI(this)
+        }
     }
 }
